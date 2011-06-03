4623d8d3329a8b0c05ae7e576b04049b1741e7f8
Sergio Ramirez <sergio.ramirez@the-cocktail.com>
2011-06-03 12:45:37 +0200

    Change routes to reflect neutrino paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.namespace('tolk') do |tolk|
-    tolk.root :controller => 'locales'
+  map.namespace('tolk', :path_prefix=>"admin/tolk") do |tolk|
+    tolk.root :controller => 'locales', :action=>:index
     tolk.resources :locales, :member => {:all => :get, :updated => :get}
     tolk.resource :search
   end
