c265ca417709aa773b1553ac0d230d568bd5ff30
Alfredo <alfredo.solano@the-cocktail.com>
2012-01-31 18:28:24 +0100

    incluimos modulos neutrino en AppContr tolk

--- app/controllers/tolk/application_controller.rb
+++ app/controllers/tolk/application_controller.rb
@@ -1,17 +1,31 @@
 module Tolk
-  class ApplicationController < ActionController::Base
-    helper :all
-    protect_from_forgery
+ class ApplicationController < ActionController::Base
+   
+   include ExceptionNotification::Notifiable
+   include NeutrinoFunctions
+   
+   # helper :application
+   
+   helper :all
+   protect_from_forgery
+   
+   acts_as_time_machine
+   
+   cattr_accessor :authenticator
+   before_filter :authenticate
 
-    cattr_accessor :authenticator
-    before_filter :authenticate
+   def authenticate
+     self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
+   end
 
-    def authenticate
-      self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
-    end
+   def ensure_no_primary_locale
+     redirect_to tolk_locales_path if @locale.primary?
+   end
+   
+   def logged_in?
+     !!current_user
+   end
 
-    def ensure_no_primary_locale
-      redirect_to tolk_locales_path if @locale.primary?
-    end
-  end
+   
+ end
 end
