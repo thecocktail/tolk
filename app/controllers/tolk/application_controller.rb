f0f8acb51c61bf032d04570bc36281e000cc6e1b
Alfredo <alfredo.solano@the-cocktail.com>
2012-06-06 17:03:28 +0200

    metemos compatibilidad con permisos multisite de IBE

--- app/controllers/tolk/application_controller.rb
+++ app/controllers/tolk/application_controller.rb
@@ -13,11 +13,24 @@ module Tolk
    
    cattr_accessor :authenticator
    before_filter :authenticate
-
+   before_filter :set_admin_site
+   
    def authenticate
      self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
    end
 
+   def set_admin_site
+     @admin_site ||= if params[:admin_site]
+       site_nicetitle = params[:admin_site]
+       site = Site.find_by_nicetitle site_nicetitle
+       session[:admin_site] = site.id
+       site
+     elsif session[:admin_site]
+       Site.find session[:admin_site]
+     else
+       set_site
+     end
+   end
    def ensure_no_primary_locale
      redirect_to tolk_locales_path if @locale.primary?
    end
